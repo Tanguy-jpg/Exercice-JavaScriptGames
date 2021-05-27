fdc2e565a770a3541666ab40d3380574fd7587b5
Tanguy-jpggit config –listgit config --global user.name Tanguy-jpg <TanguyCarnot@gmail.com>
2021-05-27 15:07:12 +0200

    Morpion clic fonctionnels

--- js/script-morpion.js
+++ js/script-morpion.js
@@ -19,50 +19,136 @@
 // document.getElementById("button1").className=nouveau;
 // }
 
+var croix = true;
 
 function button1(){
     var button1 = document.getElementById("button1");
 
     if (button1.className.match("croix") || button1.className.match("rond")){
         alert('case dejà prise')
-    } else {
+    } else if(croix == true) {
         button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
     }
 }
-
 function button2(){
     var button1 = document.getElementById("button2");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button3(){
     var button1 = document.getElementById("button3");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button4(){
     var button1 = document.getElementById("button4");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button5(){
     var button1 = document.getElementById("button5");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button6(){
     var button1 = document.getElementById("button6");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button7(){
     var button1 = document.getElementById("button7");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button8(){
     var button1 = document.getElementById("button8");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 function button9(){
     var button1 = document.getElementById("button9");
-    button1.classList.add("croix");
+
+    if (button1.className.match("croix") || button1.className.match("rond")){
+        alert('case dejà prise')
+    } else if(croix == true) {
+        button1.classList.add("croix");
+        croix = false;
+    }else if (croix == false){
+        button1.classList.add("rond");
+        croix=true;
+    }
 }
 
+var buttonA1 = document.getElementById("button1");
+var buttonA2 = document.getElementById("button2");
+var buttonA3 = document.getElementById("button3");
+var buttonB1 = document.getElementById("button4");
+var buttonB2 = document.getElementById("button5");
+var buttonB3 = document.getElementById("button6");
+var buttonC1 = document.getElementById("button7");
+var buttonC2 = document.getElementById("button8");
+var buttonC3 = document.getElementById("button9");
 
-
-
+if (buttonA1.className.match(" button rond") && buttonA2.className.match("button rond") && buttonA3.className.match("button rond") ){
+    alert("RondWin!");
+}
